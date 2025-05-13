094fff29aaa8816f374a765b43318832528cebfb
nonyonah <nonyonah@gmail.com>
2025-05-13 17:49:58 +0100

    Implement wallet connection modal and update Mono icon

--- src/app/auth/account-connection/page.tsx
+++ src/app/auth/account-connection/page.tsx
@@ -101,11 +101,13 @@ export default function AccountConnectionPage() {
                 
                 <div className="flex items-center justify-center text-xs text-gray-500 mt-1">
                   <span>powered by</span>
-                  <svg width="16" height="16" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-1">
-                    <path d="M33.75 6.25H6.25V33.75H33.75V6.25Z" fill="#182CD1"/>
-                    <path d="M16.0156 28.125C14.375 28.125 13.125 26.875 13.125 25.2344V20.3125C13.125 18.6719 14.375 17.4219 16.0156 17.4219H23.9844C25.625 17.4219 26.875 18.6719 26.875 20.3125V25.2344C26.875 26.875 25.625 28.125 23.9844 28.125H16.0156Z" fill="white"/>
-                    <path d="M16.0156 15.625C14.375 15.625 13.125 14.375 13.125 12.7344V11.875C13.125 10.2344 14.375 8.98438 16.0156 8.98438H23.9844C25.625 8.98438 26.875 10.2344 26.875 11.875V12.7344C26.875 14.375 25.625 15.625 23.9844 15.625H16.0156Z" fill="white"/>
-                  </svg>
+                  <img 
+                    src="/mono (2).svg" 
+                    alt="Mono" 
+                    width="16" 
+                    height="16" 
+                    className="ml-1" 
+                  />
                 </div>
                 
                 <button 
