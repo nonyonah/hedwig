51c514cb9b7d84284ad5407b49a1ce5a21543bc9
nonyonah <nonyonah@gmail.com>
2025-06-09 17:49:40 +0100

    Update overview layout and metadata pages

--- src/app/overview/layout.tsx
+++ src/app/overview/layout.tsx
@@ -1,15 +1,15 @@
-// 'use client';
+'use client';
 
-// interface DashboardLayoutProps {
-//   children: React.ReactNode;
-// }
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
 
-// export default function DashboardLayout({ children }: DashboardLayoutProps) {
-//   return (
-//     <div className="flex min-h-screen w-full overflow-x-hidden">
-//       <main className="flex-1 overflow-y-auto">
-//         {children}
-//       </main>
-//     </div>
-//   );
-// }
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <div className="flex min-h-screen w-full overflow-x-hidden">
+      <main className="flex-1 overflow-y-auto">
+        {children}
+      </main>
+    </div>
+  );
+}
